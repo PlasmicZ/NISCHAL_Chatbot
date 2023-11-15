3dd01e900e4686987ddeb4772b55118c87644fb9
PlasmicZ <119068217+PlasmicZ@users.noreply.github.com>
2023-11-16 02:23:42 +0530

    fixed the templating to give consistent replies

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ load_dotenv(find_dotenv())
 model_id = "tiiuae/falcon-7b-instruct"
 model = HuggingFaceHub(huggingfacehub_api_token=os.environ["HUGGINGFACEHUB_API_TOKEN"], repo_id=model_id,model_kwargs={"temperature":0.6,"max_new_tokens":100})
 
-template = '''never say you are an ai model,You are a polite customer service assistant ,tell people if they are being scammed or not, must help prevent scam by giving relevant replies to scam victims or potential scam victims,if they are being scammed ask them to cut off contact and request help from cybersecurity branches
-{query}
-'''
+template = "As a customer service assistant, I am committed to providing continuous support. If you suspect you might be involved in a scam, I'm here to help. I'll guide you on identifying potential scams and offer relevant advice to prevent further risks. If you find yourself in the midst of a scam, please discontinue communication immediately and seek assistance from cybersecurity branches. I'm dedicated to ensuring your safety and security. How can I assist you today? {query}"
 
 prompt = PromptTemplate(template=template, input_variables=['query'])
 conv_chain = LLMChain(llm=model, prompt=prompt, verbose=True)
 
-print(conv_chain.run("Someone is asking for my otp do i give it to them?"))
+print(conv_chain.run("someone is asking to meetup in real life to give me money what do i do?"))
