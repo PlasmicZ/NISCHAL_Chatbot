c1b82e88450720d38ced87c2fe1d68d7eca4fc3c
PlasmicZ <119068217+PlasmicZ@users.noreply.github.com>
2023-11-17 20:13:59 +0530

    Revert "final bot for now,failed routerchain,make API"
    This reverts commit 4b21b59c45bf125f02d253b962df4b8aaa1525f8.

--- ScamReport.py
+++ ScamReport.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel, Field
 
 class ScamReport(BaseModel):
     is_scam : bool = Field (description="Is the person being scammed? ")
+    support_request : str = Field(description="What kind of scam is the person facing?")
     in_danger: bool = Field(description="Is the person in danger?")
     been_scammed: bool = Field(description="has the scam happened already?")
-    response : str = Field(description="What should the person do?")
--- main.py
+++ main.py
@@ -11,16 +11,15 @@ load_dotenv(find_dotenv())
 model_id = "HuggingFaceH4/zephyr-7b-alpha"
 model = HuggingFaceHub(huggingfacehub_api_token=os.environ["HUGGINGFACEHUB_API_TOKEN"], repo_id=model_id, model_kwargs={"temperature": 0.8, "max_new_tokens": 250})
 
-template = '''analyze user input, provide accurate JSON responses only, operate as a customer service AI, strictly adhere to formatting instructions,
-user_query:\n{question}\n{formatting_instructions}
-If the user query is related to personal matters, provide a helpful and non-misleading response. Do not classify benign inquiries as scams."
+template = '''ONLY GIVE SINGLE JSON OUTPUT NO OTHER INFORMATION,As a customer service assistant, I am committed to providing continuous support. If you suspect you might be involved in a scam, I'm here to help. I'll guide you on identifying potential scams and offer relevant advice to prevent further risks. If you find yourself in the midst of a scam, please discontinue communication immediately and seek assistance from cybersecurity branches. I'm dedicated to ensuring your safety and security. How can I assist you today? 
+text:\n{question}\n{formatting_instructions}
 '''
 parser = PydanticOutputParser(pydantic_object=ScamReport)
 
 prompt = PromptTemplate(template=template,input_variables = ["question"] , partial_variables={"formatting_instructions": parser.get_format_instructions()})
 
 
-_input = prompt.format_prompt(question="ALMONDS: Are almonds a scam?")
+_input = prompt.format_prompt(question="someone is asking for my otp, do i give it to them?")
 output=model(_input.to_string())
 reply=parser.parse(output)
-print("\nCHATBOT OUTPUT: \n",reply)
+print("CHATBOT OUTPUT: \n ",reply)
