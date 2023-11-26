79b8c62891332e7e58a724a3c111a11848d07d8a
PlasmicZ <aviralkamaljain@gmail.com>
2023-11-26 12:49:36 +0530

    switched to orca-mini and simplified chatbot task

--- ScamReport.py
+++ ScamReport.py
@@ -2,6 +2,3 @@ from pydantic import BaseModel, Field
 
 class ScamReport(BaseModel):
     is_scam : bool = Field (description="Is the person being scammed? ")
-    support_request : str = Field(description="What kind of scam is the person facing?")
-    in_danger: bool = Field(description="Is the person in danger?")
-    been_scammed: bool = Field(description="has the scam happened already?")
--- main.py
+++ main.py
@@ -1,25 +1,30 @@
-from dotenv import load_dotenv, find_dotenv
-import os
-from langchain import HuggingFaceHub, LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import PydanticOutputParser
 from ScamReport import ScamReport
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import Ollama
 
 
-load_dotenv(find_dotenv())
 
-model_id = "HuggingFaceH4/zephyr-7b-alpha"
-model = HuggingFaceHub(huggingfacehub_api_token=os.environ["HUGGINGFACEHUB_API_TOKEN"], repo_id=model_id, model_kwargs={"temperature": 0.8, "max_new_tokens": 250})
 
-template = '''ONLY GIVE SINGLE JSON OUTPUT NO OTHER INFORMATION,As a customer service assistant, I am committed to providing continuous support. If you suspect you might be involved in a scam, I'm here to help. I'll guide you on identifying potential scams and offer relevant advice to prevent further risks. If you find yourself in the midst of a scam, please discontinue communication immediately and seek assistance from cybersecurity branches. I'm dedicated to ensuring your safety and security. How can I assist you today? 
-text:\n{question}\n{formatting_instructions}
+
+llm = Ollama(
+    model="orca-mini:13b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+)
+
+template =''' Strictly reply only in JSON. Your name is Nischal, and you engage in direct customer interactions. Analyze if customers are attempting casual conversation mark spam as false and reply kindly. If they explicitly ask for help, perform a scam analysis and respond accordingly.
+Text:
+{question}
+{formatting_instructions}
+
 '''
 parser = PydanticOutputParser(pydantic_object=ScamReport)
 
 prompt = PromptTemplate(template=template,input_variables = ["question"] , partial_variables={"formatting_instructions": parser.get_format_instructions()})
 
 
-_input = prompt.format_prompt(question="someone is asking for my otp, do i give it to them?")
-output=model(_input.to_string())
+_input = prompt.format_prompt(question='''someone is asking for my bank details ''')
+output=llm(_input.to_string())
 reply=parser.parse(output)
 print("CHATBOT OUTPUT: \n ",reply)
