f5963dbe3ec62e3c0528ecc6daa6d74acb7734f8
PlasmicZ <aviralkamaljain@gmail.com>
2023-11-28 01:35:24 +0530

    complete api is done, will use with portforwarding

--- ScamReport.py
+++ ScamReport.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, Field
 
 class ScamReport(BaseModel):
-    is_scam : bool = Field (description="Is the person being scammed or is the person just talking to you? ")
     response : str = Field (description="reply like a chatbot")
--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+from flask import Flask, request, jsonify
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import PydanticOutputParser
-from ScamReport import ScamReport
+from ScamReport import ScamReport  # Make sure the import path is correct
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.llms import Ollama
+import json
 
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ScamReport):
+            # Convert ScamReport object to a dictionary
+            return obj.dict()
+        return super().default(obj)
 
-
-
+app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
 
 llm = Ollama(
-    model="orca-mini:13b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]) , temperature = 0.1
+    model="orca-mini:13b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), temperature=0.1
 )
 
-template =''' Strictly reply only in JSON. Your name is Nischal, and you engage in direct customer interactions. Analyze if customers are attempting casual conversation mark spam as false and reply kindly. If they explicitly ask for help, perform a scam analysis and respond accordingly.
+template = ''' Strictly reply only in JSON. Your name is Nischal, and you engage in direct customer interactions. Analyze if customers are attempting casual conversation mark spam as false and reply kindly. If they explicitly ask for help, perform a scam analysis and respond accordingly.
 Text:
 {question}
 {formatting_instructions}
@@ -21,10 +29,20 @@ Text:
 '''
 parser = PydanticOutputParser(pydantic_object=ScamReport)
 
-prompt = PromptTemplate(template=template,input_variables = ["question"] , partial_variables={"formatting_instructions": parser.get_format_instructions()})
+prompt = PromptTemplate(template=template, input_variables=["question"],
+                        partial_variables={"formatting_instructions": parser.get_format_instructions()})
 
 
-_input = prompt.format_prompt(question='''hi''')
-output=llm(_input.to_string())
-reply=parser.parse(output)
-print("CHATBOT OUTPUT: \n ",reply)
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    if 'question' in data:
+        _input = prompt.format_prompt(question=data['question'])
+        output = llm(_input.to_string())
+        reply = parser.parse(output)
+        return jsonify({'response': reply})
+    else:
+        return jsonify({'error': 'No question provided'})
+
+if __name__ == '__main__':
+    app.run(debug=True) 
